47251302279bcb7a14ea783cc2d9adc739d15c71
WimOn <33762603+Wimonder@users.noreply.github.com>
2020-08-27 22:19:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,18 +16,15 @@ yay -S ${packages[@]} --noconfirm --needed
 # Install oh my fish
 # curl -L https://get.oh-my.fish | fish
 curl -L https://get.oh-my.fish > install
-fish install --path=~/.local/share/omf --config=~/.config/omf
-# Setup fish
-# omf install bobthefish # Theme
-
+./install --noninteractive --path=~/.local/share/omf --config=~/.config/omf
+rm -f ./install
 # Neovim setup
 # Install neovim python module
 pip3 install --user neovim
 # Install vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Setup vim-plug plugins
-vim +'PlugInstall --sync' +qa
+nvim +'PlugInstall --sync' +qa
 
 # Configure and provision i3blocks
 [ ! -d "$HOME/.config/i3blocks" ] && mkdir -p "$HOME/.config/i3blocks"
